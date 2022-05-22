62c2a953be01f441e0e4f85f7fb8881119ad5fd2
Jordan MacInnis <macinnis11@gmail.com>
2022-05-22 12:23:11 -0300

    Link main.js to index file

---
+++ main.js
@@ -0,0 +1,5 @@
+const email =document.querySelector('#email');
+const phone = document.querySelector('#phone');
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirm-password');
+ 
