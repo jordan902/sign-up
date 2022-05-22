0ce95bafa4dc68400d99c5b40c48cdffe6d9af9b
Jordan MacInnis <macinnis11@gmail.com>
2022-05-22 12:55:10 -0300

    remove input error message when focused on

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const firstName = document.querySelector('#firstName');
+const lastName = document.querySelector('#lastName');
 const email =document.querySelector('#email');
 const phone = document.querySelector('#phone');
 const password = document.querySelector('#password');
@@ -6,3 +8,14 @@ const errorMessage = document.querySelector('#errorMessage');
 
 const inputs = [email, phone, password, confirmPassword]
 
+inputs.forEach((item) => {
+    item.addEventListener("focusin", () => {
+      errorMessage.textContent = "";
+      item.classList.remove("error");
+      if (item == password || item == confirmPassword) {
+        password.classList.remove("error");
+        confirmPassword.classList.remove("error");
+      }
+    });
+  });
+  
