2128677a13c66560499e2f48ce07a38d49fddfe5
Jordan MacInnis <macinnis11@gmail.com>
2022-05-22 12:32:28 -0300

    add errorMessage variable with query selector

--- main.js
+++ main.js
@@ -2,4 +2,5 @@ const email =document.querySelector('#email');
 const phone = document.querySelector('#phone');
 const password = document.querySelector('#password');
 const confirmPassword = document.querySelector('#confirm-password');
- 
+const errorMessage = document.querySelector('#errorMessage');
+
